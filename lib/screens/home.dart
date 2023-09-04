80a8d4a9efb6cd4a901bd9100d2697b5508fd4c4
Het Prajapati <hetprajapati80@gmail.com>
2023-09-04 19:55:09 +0530

    Initial Commit 04/09/23

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,10 +1,116 @@
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+  HomeScreen({super.key});
+
+  final List catName = [
+    "Category",
+    'Classes',
+    'Free Course',
+    'BookStore',
+    'Live Course',
+    'LeaderBoard',
+  ];
+
+  final List<Color> catColor = [];
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: ListView(
+        children: [
+          Container(
+            padding: const EdgeInsets.fromLTRB(15, 15, 15, 10),
+            decoration: const BoxDecoration(
+              color: Color(0xFF674AEF),
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(20),
+                bottomRight: Radius.circular(20),
+              ),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Icon(
+                      Icons.dashboard,
+                      size: 30,
+                      color: Colors.white,
+                    ),
+                    Icon(
+                      Icons.notifications,
+                      size: 30,
+                      color: Colors.white,
+                    )
+                  ],
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                const Padding(
+                  padding: EdgeInsets.only(
+                    left: 3,
+                    bottom: 15,
+                  ),
+                  child: Text(
+                    'Hi, Programmer',
+                    style: TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.w600,
+                      letterSpacing: 1,
+                      wordSpacing: 2,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.only(top: 5, bottom: 20),
+                  width: MediaQuery.of(context).size.width,
+                  height: 55,
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: 'Search here....',
+                      hintStyle: TextStyle(
+                        color: Colors.black.withOpacity(0.5),
+                      ),
+                      prefixIcon: const Icon(
+                        Icons.search,
+                        size: 25,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 20, left: 15, right: 15),
+            child: Column(
+              children: [
+                GridView.builder(
+                  shrinkWrap: true,
+                  physics: const NeverScrollableScrollPhysics(),
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 3,
+                    childAspectRatio: 1.1,
+                  ),
+                  itemBuilder: (context, index) {
+                    return const Column();
+                  },
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
